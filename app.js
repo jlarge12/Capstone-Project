66367566f76dd105a4b6705c786b53238692901b
lvkq82 <43930598+lvkq82@users.noreply.github.com>
2023-11-25 19:16:38 +0000

    alow reset of leaderboard

--- app.js
+++ app.js
@@ -7,23 +7,18 @@ const dbConfig = require('./dbConfig');
 const app = express();
 const port = 3000;
 
-// Use cors middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 const pool = mysql.createPool(dbConfig);
 
-// Set up a route to handle GET requests
 app.get('/api/getUsers', (req, res) => {
-  console.log("Fetching user data...")
-
     pool.query('SELECT * FROM users', (queryErr, results) => {
       if (queryErr) {
         console.error('Error executing query:', queryErr);
         return res.status(500).json({ error: 'Internal Server Error' });
       }
 
-      console.log("User data loaded")
       res.json(results);
     });
 });
@@ -31,7 +26,6 @@ app.get('/api/getUsers', (req, res) => {
 app.post('/api/updateUser', (req, res) => {
   const { userId, name, email, activity } = req.body;
 
-  // Validate the data (add your own validation logic as needed)
   if (!userId || !name || !email || !activity) {
     return res.status(400).json({ error: 'Invalid data. All fields are required.' });
   }
@@ -39,7 +33,6 @@ app.post('/api/updateUser', (req, res) => {
   const activityPoints = {
     recycled_plastic: 5,
     took_the_bus: 3,
-    // Add other activities and their corresponding points
   };
 
   const points = activityPoints[activity];
@@ -53,7 +46,6 @@ app.post('/api/updateUser', (req, res) => {
     points = points + VALUES(points)
   `;
 
-  // Insert the data into the database
   pool.query(sql, [userId, name, email, points], (err, results) => {
     if (err) {
       console.error('Error adding user to the database:', err);
@@ -62,6 +54,20 @@ app.post('/api/updateUser', (req, res) => {
 
     console.log('User added to the database:', results);
     res.json({ message: 'User added successfully' });
+  });
+});
+
+app.post('/api/resetLeaderboard', (req, res) => {
+  const sql = 'DELETE FROM users';
+
+  pool.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error resetting leaderboard:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    console.log('Leaderboard reset successfully');
+    res.json({ message: 'Leaderboard reset successfully' });
   });
 });
 
